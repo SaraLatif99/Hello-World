d6f676e05c5a24dec8c487d08dced909f6b921b3
Sara Latif <32573838+SaraLatif99@users.noreply.github.com>
2020-02-17 00:55:00 +0500

    modified the mobile.java file inside First Branch /FB

--- Mobile.java
+++ Mobile.java
@@ -3,6 +3,8 @@ class Mobile{
     public static  void public static void main(String[] args) {
 
         System.out.System.out.println("Hello World");
+        System.out.System.out.println("Hi World");
+
 
     }
 
